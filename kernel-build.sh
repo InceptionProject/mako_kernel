66e596930de4c4d8d356c2997007b349240a91fd
InceptionProject <inceptionproject1911@gmail.com>
2014-07-12 18:20:01 +0530

    Simple build script

---
+++ kernel-build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#  Inception
+#
+
+clear
+
+echo ""
+echo ""
+echo "Started Hacking Your Dreams"
+echo ""
+echo ""
+
+git checkout kk
+
+make clean
+make mrproper
+export PATH=$PATH:~/toolchain/arm-eabi-4.10/bin
+export ARCH=arm
+export CROSS_COMPILE=arm-eabi-
+export ENABLE_GRAPHITE=true
+make inception_defconfig
+make -j6
+
+echo ""
+echo ""
+echo "Your Dreams Are Hacked"
+echo ""
+echo ""
