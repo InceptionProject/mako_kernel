a46f1f5b4b19af243c4bea0a61e2190a1e424fa9
InceptionProject <inceptionproject1911@gmail.com>
2014-07-13 13:30:57 +0530

    Update build script. Use any-kernel format

--- kernel-build.sh
+++ kernel-build.sh
@@ -4,10 +4,9 @@
 #
 
 rm arch/arm/boot/zImage
-rm boot.img
 rm kernel.log
-rm zip/boot.img
-rm zip/Inception.kernel-nightly.zip
+rm any-kernel/kernel/zImage
+rm any-kernel/Inception.kernel-nightly.zip
 
 clear
 
@@ -28,41 +27,18 @@ make inception_defconfig
 time make -j6 2>&1 | tee kernel.log
 
 echo ""
-echo "Building boot.img"
-cp arch/arm/boot/zImage ../ramdisk_mako/
-
-cd ../ramdisk_mako/
-
-echo ""
-echo "building ramdisk"
-./mkbootfs ramdisk | gzip > ramdisk.gz
-echo ""
-echo "making boot image"
-./mkbootimg --kernel zImage --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677 user_debug=31' --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x01600000 --ramdisk ramdisk.gz --output ../mako/boot.img
-
-rm -rf ramdisk.gz
-rm -rf zImage
-
-cd ../mako/
+echo "Inception"
+cp arch/arm/boot/zImage any-kernel/kernel
 
 zipfile="Inception.kernel-nightly.zip"
 echo ""
 echo "zipping kernel"
-cp boot.img zip/
 
-rm -rf ../ramdisk_mako/boot.img
-
-cd zip/
+cd any-kernel/
 rm -f *.zip
 zip -r -9 $zipfile *
 rm -f /tmp/*.zip
 cp *.zip /tmp
-
-cd ..
-rm arch/arm/boot/zImage
-rm boot.img
-rm kernel.log
-rm zip/boot.img
 
 echo ""
 echo ""
